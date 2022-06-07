e49ec5fb9effc6d1fb36d5d644dfe91534fa6c15
wanyoung <dhks2869@gmail.com>
2022-06-07 16:32:13 +0900

    ADD:theme.js

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const colors = {
+  black: '#000000',
+  grey: '#999999',
+  green: '#3cb46e',
+  blue: '#000080',
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
