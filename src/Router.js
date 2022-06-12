fb174ab898718a65bcf87fdf647350eb23504aa9
KIMHYESU <kimss9209@gmail.com>
2022-06-12 13:46:32 +0900

    Modify: 장바구니 기능 구현 완료 - PR review 수정요청사항 반영 - 체크박스 클릭 시 opacity값 변화 - 체크박스 체크 된 상품만 금액 변경 - 체크박스 클릭 시 클릭 순서대로 Array에 담김 - 쓰레기통 버튼 클릭 시 Array값 undefined, tempcart 페이지 보여줌

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Main from './pages/Main';
 import Cart from './pages/Cart/Cart';
-import TempCart from './pages/Cart/TempCart';
+import EmptyCart from './pages/Cart/EmptyCart';
 import Products from './pages/products/Products';
 import ProductDetail from './pages/productDetail/ProductDetail';
 import Survey from './pages/survey/Survey';
@@ -18,7 +18,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/tempcart" element={<TempCart />} />
+        <Route path="/EmptyCart" element={<EmptyCart />} />
         <Route path="/product" element={<Products />} />
         <Route path="/product/detail" element={<ProductDetail />} />
         <Route path="/survey" element={<Survey />} />
--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -2,41 +2,49 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import CartHeader from '../../components/Cart/CartHeader';
 import ProductList from '../../components/Cart/ProductList';
-import Total from '../../components/Cart/Total';
 import Payment from '../../components/Cart/Payment';
+import EmptyCart from './EmptyCart';
 
 const Cart = () => {
   const [itemListValue, setItemListValue] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [check, setCheck] = useState([]);
+
+  const clear = () => {
+    setItemListValue();
+  };
+
   useEffect(() => {
-    fetch('data/productList.json', {
-      method: 'GET',
-    })
+    fetch('data/productList.json')
       .then(res => res.json())
       .then(result => {
         setItemListValue(result);
       });
   }, []);
 
-  return (
+  return itemListValue ? (
     <CartWrapper>
       <Content>
-        <CartHeader />
-        <ProductLists1>
-          <h3>정기구독 제품</h3>
-        </ProductLists1>
+        <CartHeader clear={clear} />
+        <h3>정기구독 제품</h3>
         {itemListValue.map(itemlist => (
-          <ProductList itemlist={itemlist} key={itemlist.id} />
+          <ProductList
+            itemlist={itemlist}
+            key={itemlist.id}
+            setCheck={setCheck}
+            check={check}
+            setTotalPrice={setTotalPrice}
+          />
         ))}
-        <ProductLists1>
-          <h3>1회 구매 제품</h3>
-        </ProductLists1>
-        {itemListValue.map(itemlist => (
-          <ProductList itemlist={itemlist} key={itemlist.id} />
-        ))}
-        <Total />
-        <Payment />
+        <TotalAmount>
+          <p>총 결제금액</p>
+          <p>{totalPrice.toLocaleString()}원</p>
+        </TotalAmount>
+        <Payment check={check} />
       </Content>
     </CartWrapper>
+  ) : (
+    <EmptyCart />
   );
 };
 
@@ -44,8 +52,7 @@ const CartWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 1024px;
-  height: 200vh;
-  background-color: white;
+  height: 150vh;
   margin: 0 auto;
   padding: 0 137px;
   color: black;
@@ -54,12 +61,21 @@ const CartWrapper = styled.div`
 const Content = styled.div`
   h3 {
     padding-bottom: 1.3rem;
-    border-bottom: 1px solid #e0e0e0;
+    border-bottom: 0.063px solid #e0e0e0;
     font-size: 1.2rem;
     font-weight: 700;
     margin-top: 5rem;
   }
 `;
-const ProductLists1 = styled(Content)``;
+
+const TotalAmount = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border-top: 0.12rem solid grey;
+  margin-bottom: 5rem;
+  padding-top: 2rem;
+  font-size: 1.5rem;
+  font-weight: 800;
+`;
 
 export default Cart;
